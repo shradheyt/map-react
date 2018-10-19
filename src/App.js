cdae35dd86150e94c7e2a119aa6fc4fba509a1bb
shradheyt <shradhey029tripathi@gmail.com>
2018-10-19 23:02:41 +0530

    First Commit- Basic react app with map completed

--- src/App.js
+++ src/App.js
@@ -3,26 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+componentDidMount() {
+  this.renderMap();
+}
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCpAwOhD9MaX9RNinoKze2fnvKSCO8TKT0&callback=initMap");
+    window.initMap = this.initMap;
+  }
+
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 1
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map"></div>
+      </main>
+      
     );
   }
+}
+
+/*
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script>
+*/
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
